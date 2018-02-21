304bc8e06cc0c338af798ef9ce5cb795485b2500
jrojas <jrojas@neramirez-desktop.koghi.com>
2018-02-21 09:47:04 -0500

    Se eliminan archivos que no se usan , se agregan variables de entorno en front

--- HotelFront/src/app/services/constants.ts
+++
@@ -1,3 +0,0 @@
-export const Constants = {
-  URL: 'http://localhost:3020/api'
-}
--- HotelFront/src/app/services/hotels.service.ts
+++ HotelFront/src/app/services/hotels.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, RequestOptions } from '@angular/http';
-import { Constants } from './index';
 import 'rxjs/add/operator/map';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class HotelsService {
@@ -9,9 +9,11 @@ export class HotelsService {
   public filterParams: any;
   hotels: any[] = [];
   constructor(private http: Http) {
-    this.url = Constants.URL;
+    //Se obtiene la URL del API desde las variables de entorno
+    this.url = environment.URL;
   }
 
+  //Método que se comunica con el API para obtener la lista de hoteles
   getHotels(params) {
     return this.http.post(this.url + '/getHotels', params).map((res: any) => {
       res = res.json();
